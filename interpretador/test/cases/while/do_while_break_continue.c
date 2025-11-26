19da0f88a970c8bae48b661b4f13f3453c9a3475
guinuto2 <guiguievangelista22@gmail.com>
2025-11-26 11:53:16 -0300

    UPDATE : testes de break e continue nos laços

---
+++ interpretador/test/cases/while/do_while_break_continue.c
@@ -0,0 +1,18 @@
+int main()
+{
+    int i = 0;
+    do
+    {
+        if (i == 3)
+        {
+            i++;
+            continue;
+        }
+        if (i == 8)
+        {
+            break;
+        }
+        i;
+        i++;
+    } while (i < 15);
+}
--- interpretador/test/test_5_while.py
+++ interpretador/test/test_5_while.py
@@ -3,11 +3,13 @@ import pytest
 from glob import glob
 
 test_files = sorted(glob(f"{tests_path}/while/*.c"))
-expected_return_code = [0, 0, 0]  # while, do_while, no_block
+expected_return_code = [0, 0, 0]  #  do_while_break_continue,while, do_while, no_block
 expected_output = [
+    "[DEBUG] Valor: 7",
     "[DEBUG] Valor: 3",
     "[DEBUG] Valor: 3",
     "[DEBUG] Valor: 3",
+    "[DEBUG] Break na linha 13",
 ]
 
 
